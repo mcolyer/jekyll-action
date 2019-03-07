5132b994ea9a8c3e7c284bed67df79ac6cdf80b6
Matt Colyer <matt@colyer.name>
2019-03-07 12:17:27 -0800

    Remove pages publishing

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,27 +18,3 @@ echo "Source for the Jekyll site is set to ${SRC}"
 bundle exec jekyll build -s ${SRC} -d build
 echo "#################################################"
 echo "Jekyll build done"
-
-cd build
-
-# No need to have GitHub Pages to run Jekyll
-touch .nojekyll
-
-echo "#################################################"
-echo "Now publishing"
-if [[ -z "${JEKYLL_PAT}" ]]; then
-  TOKEN=${GITHUB_TOKEN}
-else 
-  TOKEN=${JEKYLL_PAT}
-fi
-
-remote_repo="https://${TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
-remote_branch="gh-pages" && \
-git init && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add . && \
-git commit -m 'jekyll build from Action' && \
-git push --force $remote_repo master:$remote_branch && \
-rm -fr .git && \
-cd ..
